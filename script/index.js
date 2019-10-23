6bc526f1deb3a472573b154836c8f828c8aba576
evan-hsiao <evan-hsiao@users.noreply.github.com>
2019-10-23 17:02:48 +0800

    class update

---
+++ script/index.js
@@ -0,0 +1,8 @@
+ 
+        var mobile_nav_toggle = document.querySelector(".mobile_nav_toggle");
+        var mobile_nav = document.querySelector(".mobile_nav");
+
+        mobile_nav_toggle.onclick = function () {
+            mobile_nav.classList.toggle("active");
+            
+        }
