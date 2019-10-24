42c819a1ae7e449123b73e715a529a28b6c7d409
evan-hsiao <evan-hsiao@users.noreply.github.com>
2019-10-24 17:39:37 +0800

    update
    update at home

--- script/index.js
+++ script/index.js
@@ -1,8 +1,6 @@
- 
-        var mobile_nav_toggle = document.querySelector(".mobile_nav_toggle");
-        var mobile_nav = document.querySelector(".mobile_nav");
+var mobile_nav_toggle = document.querySelector(".mobile_nav_toggle");
+var mobile_nav = document.querySelector(".mobile_nav");
 
-        mobile_nav_toggle.onclick = function () {
-            mobile_nav.classList.toggle("active");
-            
-        }
+    mobile_nav_toggle.onclick = function () {
+    mobile_nav.classList.toggle("active");
+}
