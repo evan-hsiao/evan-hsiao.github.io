b5fdc6f3f76b822bb3e43cfaa6df348f686027cb
evan-hsiao <evan-hsiao@users.noreply.github.com>
2020-01-06 19:22:04 +0800

    update

--- script/index-script.js
+++ script/index-script.js
@@ -1,21 +1,34 @@
-const form = document.getElementById("form")
-const name = document.getElementById("name")
-const password = document.getElementById("password")
-const errorElement = document.getElementById("error")
 
-form.addEventListener('submit', (e) => {
-    let messages = []
-    // 定義送出的訊息           
+// 預設使用者資訊
+var passUserInfo = [
+    {
+        name: "evan",
+        password: "123456789"
+    },
+    {
+        name: "test",
+        password: "987654321"
+    }
+]
 
-    if (name.length <= 6) {
-        messages.push('it is not longer')
+function getInfo() {
+    // 設定變數，取用id送出的值
+    var username = document.getElementById("name").value
+    var password = document.getElementById("password").value
+    var keyInfo = document.getElementById("key_info")
+    // console.log(username + "welcome!!" + password);
+
+    // 建立迴圈
+    for (i = 0; i < passUserInfo.length; i++) {
+        if (username == passUserInfo[i].name && password == passUserInfo[i].password) {
+            alert(username + " welcome歡迎光臨!!");
+            // console.log(username + " welcome歡迎光臨!!");
+            return
+        }
+        else {
+            alert("Hi " + username + " 輸入錯誤!!");
+            // console.log("Hi " + username + " 輸入錯誤!!");
+            return
+        }
     }
-    if (password.length >= 15) {
-        messages.push('it is too longer')
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        // 停止事件
-        errorElement.innerText = messages.join(', ')
-    }
-})  
+}
