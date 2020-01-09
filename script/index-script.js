89f64b2262fd33516bfa544562eff03fcc660605
evan-hsiao <evan-hsiao@users.noreply.github.com>
2020-01-09 08:49:47 +0800

    up

--- script/index-script.js
+++ script/index-script.js
@@ -16,19 +16,22 @@ function getInfo() {
     var username = document.getElementById("name").value
     var password = document.getElementById("password").value
     var keyInfo = document.querySelector(".key_info")
+    var navbar = document.querySelector(".right_navbar")
     // console.log(username + password);
 
     // 建立迴圈
     for (i = 0; i < passUserInfo.length; i++) {
         if (username == passUserInfo[i].name && password == passUserInfo[i].password) {
             keyInfo.innerText = username + "歡迎光臨!!";
+            navbar.style.display = "block";
             // console.log(username);
             return
+            
         }
         else if (username != passUserInfo[i].name) {
             keyInfo.innerText = "姓名輸入錯誤";
             return
-            
+
         }
         else if
             (password != passUserInfo[i].password) {
@@ -36,8 +39,10 @@ function getInfo() {
             return
         }
         else {
-            keyInfo.innerText = "輸入錯誤";            
+            keyInfo.innerText = "輸入錯誤";
             return
         }
+
     }
+    
 }
