c6cc4e77f5783b1cf21706fe8d3d961d4462dec3
evan-hsiao <evan-hsiao@users.noreply.github.com>
2020-03-17 07:42:11 +0800

    up

---
+++ script/index_masonryLayout.js
@@ -0,0 +1,19 @@
+
+// $.ajax({
+//     url: '',
+
+//     success: function (info) {
+
+//         var all_bibles = info.record;
+
+//         all_bibles.forEach(record => {
+//             var cname = record.cname;
+//             var version = record.version;
+
+//             $(".bible_list").append(`<div class="bible_all">
+//                  <h1>${cname} </h1>
+//                  <h2>${version} </h2>
+//              </div>`);
+//         })
+//     }
+// });
