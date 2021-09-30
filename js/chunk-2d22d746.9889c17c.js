6a1fb146d4d3e11c9fe21f299d4bff1bf8d85396
evan-hsiao <56854767+evan-hsiao@users.noreply.github.com>
2021-09-30 23:38:21 +0800

    upd

---
+++ js/chunk-2d22d746.9889c17c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about p-5"},[n("h1",{staticClass:"text-white"},[t._v("About")])])}],c=n("2877"),u={},l=Object(c["a"])(u,s,a,!1,null,null,null);e["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.9889c17c.js.map
